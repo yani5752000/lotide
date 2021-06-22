48de15c8c692a5ee18a2b178ac843bb15b08a79b
Farid Asadpour <yani5752000@yahoo.com>
2021-06-22 00:28:37 +0000

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😉😉😉Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🤔🤔🤔Assertion Failed: " + actual + " !== "  + expected);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("soe", 3);
+assertEqual(56, 34);
+assertEqual("from", "from");
