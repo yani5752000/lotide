5b4d77d8ea3d086d2fd68a11cd0def044d172ff4
Farid Asadpour <yani5752000@yahoo.com>
2021-06-22 00:58:06 +0000

    head.js

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😉😉😉Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤔🤔🤔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  return arr[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([3]), 3);
+assertEqual(head([]), undefined);
