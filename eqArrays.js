44f5abad8bd2c166f6788c8d1d6c15cb942c4740
Farid Asadpour <yani5752000@yahoo.com>
2021-06-22 23:06:04 +0000

    eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😉😉😉Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤔🤔🤔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(a1, a2) {
+  if (a1.length !== a2.length) {
+    return false;
+  }
+
+  for(let i = 0; i < a1.length; i++) {
+    if (a1[i] !== a2[i]){
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
