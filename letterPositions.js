28a60ac5f4623cd6b9e27e9249849ac6bdb6d92d
Farid Asadpour <yani5752000@yahoo.com>
2021-06-23 22:27:59 +0000

    letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+const letterPositions = function(str) {
+  const results = {};
+
+  for(let  i = 0; i < str.length; i++) {
+    if(str[i] !== " " ) {
+      if(results[str[i]]){
+        results[str[i]].push(i);
+      } else {
+        results[str[i]] = [i];
+      }
+    }
+  }
+
+  return results;
+};
+
+
+console.log(letterPositions("lighthouse in the house"));
+console.log(letterPositions("hello"));
