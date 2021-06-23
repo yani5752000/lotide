68d0412b130228d41b234a67d11538adde62d827
Farid Asadpour <yani5752000@yahoo.com>
2021-06-23 21:56:24 +0000

    countLetters

--- countLetters.js
+++ countLetters.js
@@ -29,19 +29,3 @@ console.log(countLetters(""));
 console.log(countLetters("  "));
 console.log(countLetters("l"));
 
-const countOnly = function(allItems, itemsToCount) {
-  const results = {};
-
-  for (const item of allItems) {
-    if (itemsToCount[item]) {
-      if (results[item]) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
-      }
-    }
-  }
-
-
-  return results;
-};
