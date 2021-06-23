9d692eeb4e0d9450405273bd5affc1860d7f2922
Farid Asadpour <yani5752000@yahoo.com>
2021-06-23 21:49:20 +0000

    countLetters

---
+++ countLetters.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😉😉😉Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤔🤔🤔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  const results = {};
+
+  for(let item of str) {
+    if(item !== " " ) {
+      if(results[item]){
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+
+console.log(countLetters("lighthouse in the house"));
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+
+  return results;
+};
