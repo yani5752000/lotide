d853153a55ca1d1ee5b5af83325e128b58654b4d
Farid Asadpour <yani5752000@yahoo.com>
2021-06-28 18:47:34 +0000

    assertArraysEqualTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(a1, a2) {
-  if (a1.length !== a2.length) {
-    return false;
-  }
-
-  for(let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]){
-      return false;
-    }
-  }
-
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -20,13 +8,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-/*const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😉😉😉Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤔🤔🤔Assertion Failed: ${actual} !== ${expected}`);
-  }
-};*/
-
-assertArraysEqual([1, 2, 5, 6], [1, 2, 5, 6]);
-assertArraysEqual([1, 2, 4, 6], [1, 2, 5, 6]);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,4 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+assertArraysEqual([1, 2, 5, 6], [1, 2, 5, 6]);
+assertArraysEqual([1, 2, 4, 6], [1, 2, 5, 6]);
