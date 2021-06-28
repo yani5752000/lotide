fcd4c714f874b936947def0a27f0f30f3bf69d39
Farid Asadpour <yani5752000@yahoo.com>
2021-06-28 21:32:14 +0000

    changing eqArraytest to previous state

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,12 +1,7 @@
-const assert = require('chai').assert;
 const eqArrays = require("../eqArrays");
+const assertEqual = require("../assertEqual");
 
-describe("#eqArrays", () => {
-  it("returns true for ([1, 2, 3], [1, 2, 3])", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-  });
-
-  it("returns false for (['5'], [5])", () => {
-    assert.strictEqual(eqArrays(['5'], [5]), false); 
-  });
-});
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
