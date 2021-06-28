8c1e63d781ce81850e0ea789ae4f0770cc5a274f
Farid Asadpour <yani5752000@yahoo.com>
2021-06-28 18:22:47 +0000

    eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😉😉😉Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤔🤔🤔Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(a1, a2) {
   if (a1.length !== a2.length) {
     return false;
@@ -20,12 +12,5 @@ const eqArrays = function(a1, a2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+module.exports = eqArrays;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
