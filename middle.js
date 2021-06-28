4430d44ba8c633205c12361c5a552bcb16e29635
Farid Asadpour <yani5752000@yahoo.com>
2021-06-28 19:03:06 +0000

    middleTest

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(a1, a2) {
-  if (a1.length !== a2.length) {
-    return false;
-  }
-
-  for(let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]){
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`😉😉😉Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤔🤔🤔Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(arr) {
   if (arr.length <= 2) {
     return [];
@@ -30,9 +8,4 @@ const middle = function(arr) {
   }
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
