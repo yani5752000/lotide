0343211a6cb15aaa9deebcd293a0cf180f67294c
Farid Asadpour <yani5752000@yahoo.com>
2021-12-28 19:41:51 +0000

    updated eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,43 @@
+const eqObjects = require("./eqObjects");
+
 const eqArrays = function(a1, a2) {
   if (a1.length !== a2.length) {
     return false;
   }
 
   for(let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]){
+    if(!Array.isArray(a1[i]) && !Array.isArray(a2[i])) {
+      if(!(a1[i] instanceof Object) && !(a2[i] instanceof Object)) {
+        if (a1[i] !== a2[i]){
+          return false;
+        }
+      }
+      if(!(a1[i] instanceof Object) && (a2[i] instanceof Object)) {
+        return false;
+      }
+      if((a1[i] instanceof Object) && !(a2[i] instanceof Object)) {
+        return false;
+      }
+      if((a1[i] instanceof Object) && (a2[i] instanceof Object)) {
+        if(!eqObjects(a1[i], a2[i])) {
+          return false;
+        }
+      }
+    }
+    if(!Array.isArray(a1[i]) && Array.isArray(a2[i])) {
       return false;
     }
+    if(Array.isArray(a1[i]) && !Array.isArray(a2[i])) {
+      return false;
+    }
+    if(Array.isArray(a1[i]) && Array.isArray(a2[i])) {
+      if(!eqArrays(a1[i], a2[i])) {
+        return false;
+      }
+    }
+    
+    
+    
   }
 
   return true;
@@ -14,4 +45,3 @@ const eqArrays = function(a1, a2) {
 
 module.exports = eqArrays;
 
-module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -5,3 +5,8 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertEqual(eqArrays([[2, [3, 5]], [4]], [[2, [3, 5]], [4]]), true);
+
